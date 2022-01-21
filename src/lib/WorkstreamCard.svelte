18e8096d971b8c087c045f680e04ed79002b0a40
Julien Donck <hello@juliendonck.com>
2022-01-21 09:46:10 +0100

    first styles + data
    Signed-off-by: Julien Donck <hello@juliendonck.com>

--- src/lib/WorkstreamCard.svelte
+++ src/lib/WorkstreamCard.svelte
@@ -1,10 +1,26 @@
 <script lang="ts">
+	import { format } from 'timeago.js';
 	export let data;
 </script>
 
 <div class="card">
-	<h3>{data.name}</h3>
-	<p>owner: {data.owner}</p>
+	<h3>{data.title}</h3>
+	<p>{data.owner}</p>
+	<p>{format(data.created)}</p>
 	<pre>{data.type}</pre>
-	<p>rate: {data.rate}/sec</p>
+	<p>{data.desc}</p>
+	<p>rate: {data.rate} {data.currency}/sec</p>
 </div>
+
+<style>
+	.card {
+		display: flex;
+		flex-direction: column;
+		padding: 1.5rem;
+		border: 1px solid var(--color-primary);
+	}
+
+	.card > * {
+		padding-top: 0.5rem;
+	}
+</style>
