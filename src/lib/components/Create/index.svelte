0f1f8fafb102e86db75dab705f38c2a26fc109ee
Julien Donck <hello@juliendonck.com>
2022-01-23 18:52:57 +0100

    fix naming

--- src/lib/components/Create/index.svelte
+++ src/lib/components/Create/index.svelte
@@ -1,13 +1,10 @@
-<script lang ts>
+<script>
 	import Modal from '@components/Modal/index.svelte';
 </script>
 
 <Modal>
 	<span slot="body"><h3>Create Workstream</h3></span>
 	<span slot="actions">
-		<button on:click>Next</button>
+		<button>Next</button>
 	</span>
 </Modal>
-
-<style>
-</style>
