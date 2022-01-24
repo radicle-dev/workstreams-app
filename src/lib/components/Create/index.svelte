86dd09ae2415d9757f0a4427ab2c42b7bf1d5298
Julien Donck <hello@juliendonck.com>
2022-01-24 17:40:34 +0100

    basic create modal steps
    Signed-off-by: Julien Donck <hello@juliendonck.com>

--- src/lib/components/Create/index.svelte
+++ src/lib/components/Create/index.svelte
@@ -1,10 +1,151 @@
-<script>
+<script lang="ts">
+	import { closeModal } from '$lib/stores/modalStore.js';
 	import Modal from '@components/Modal/index.svelte';
+	import TextInput from '$lib/shared/TextInput.svelte';
+	import Spinner from '$lib/shared/Spinner.svelte';
+
+	let type: string = 'grant';
+	let steps: number = 1;
+	let title: string;
+	let rate: string;
+	let desc: string;
+	let roleStart: string;
+	let grantDuration: string;
 </script>
 
 <Modal>
-	<span slot="body"><h3>Create Workstream</h3></span>
-	<span slot="actions">
-		<button>Next</button>
-	</span>
+	<div slot="body" class="container">
+		{#if steps === 1}
+			<span class="emoji">👔</span>
+			<h3>Create a work stream</h3>
+			<p>Start with choosing the type of workstream.</p>
+			<div class="inputs">
+				<label for="type">
+					<input type="radio" bind:group={type} name="type" value="grant" />
+					Grant
+				</label>
+				<p>
+					Creates a work stream with a fixed duration and rate. Only one single wallet can be the
+					recipient.
+				</p>
+				<label for="type">
+					<input type="radio" bind:group={type} name="type" value="role" />
+					Role
+				</label>
+				<p>
+					Creates a work stream with a start date and fixed day rate. There can be multiple people
+					working in the same role.
+				</p>
+			</div>
+			<div class="actions">
+				<button class="transparent" label="Close" on:click={$closeModal}>Close</button>
+				<button
+					on:click={() => {
+						steps = steps + 1;
+					}}>Next</button
+				>
+			</div>
+		{:else if steps === 2}
+			<span class="emoji">👔</span>
+			<h3>Create a work stream</h3>
+			<p>Describe the {type} and set a rate.</p>
+			<TextInput
+				style="margin-bottom: 2rem;"
+				label="Title"
+				placeholder="Enter a title"
+				value={title}
+			/>
+			<div class="meta">
+				<TextInput
+					style="margin-bottom: 2rem;"
+					placeholder="Enter rate per day in DAI"
+					label="Rate per day"
+					value={rate}
+				/>
+				{#if type === 'grant'}
+					<TextInput
+						style="margin-bottom: 2rem;"
+						placeholder="Enter amount of days"
+						label="Grant duration"
+						value={grantDuration}
+					/>
+				{:else if type === 'role'}
+					<TextInput
+						style="margin-bottom: 2rem;"
+						placeholder="Enter a date"
+						label="Start date"
+						value={roleStart}
+					/>
+				{/if}
+			</div>
+			<TextInput
+				style="margin-bottom: 2rem;"
+				label="Description"
+				placeholder="Enter a description (markdown supported)"
+				value={desc}
+			/>
+
+			<div class="actions">
+				<button
+					class="transparent"
+					on:click={() => {
+						steps = steps - 1;
+					}}>Back</button
+				>
+				<button
+					on:click={() => {
+						steps = steps + 1;
+					}}>Create {type}</button
+				>
+			</div>
+		{:else if steps === 3}
+			<span class="emoji">👔</span>
+			<h3>Create a work stream</h3>
+			<p>Please sign in your connected wallet</p>
+			<Spinner style="display: flex;justify-content: center;" />
+			<div class="actions">
+				<button class="transparent" label="Done" on:click={$closeModal}>Done</button>
+			</div>
+		{/if}
+	</div>
 </Modal>
+
+<style>
+	.container > * {
+		margin-bottom: 2rem;
+	}
+	.container > p {
+		color: var(--color-teal);
+	}
+
+	h3 {
+		margin-top: 1rem;
+	}
+
+	.inputs {
+		background-color: var(--color-teal-darker);
+		padding: 1.5rem;
+		text-align: left;
+		border-radius: 1rem;
+	}
+
+	input {
+		margin-bottom: 1rem;
+	}
+	p:first-of-type {
+		margin-bottom: 2rem;
+	}
+
+	.meta {
+		display: flex;
+		gap: 1.5rem;
+	}
+
+	.actions {
+		display: flex;
+		justify-content: center;
+		padding-top: 0.5rem;
+		margin: 2rem 0 0;
+		gap: 0.75rem;
+	}
+</style>
--- src/lib/components/Modal/index.svelte
+++ src/lib/components/Modal/index.svelte
@@ -1,5 +1,4 @@
-<script lang="ts">
-	import { closeModal } from '$lib/stores/modalStore.js';
+<script>
 </script>
 
 <div class="modal-overlay" />
@@ -7,11 +6,6 @@
 <div class="modal-floating">
 	<div class="modal">
 		<slot name="body" />
-
-		<div class="modal-actions">
-			<button label="Cancel" on:click={$closeModal}>close</button>
-			<slot name="actions" />
-		</div>
 	</div>
 </div>
 
@@ -36,18 +30,14 @@
 		background-color: rgba(0, 0, 0, 0.75);
 	}
 	.modal {
-		padding: 2rem 3rem;
+		margin: 1.5rem;
+		padding: 2.5rem;
 		border: 1px solid var(--color-teal-dark);
 		background: var(--color-background);
 		box-shadow: 0.5rem 0.5rem 4rem var(--color-teal-dark);
-		min-width: 30rem;
-		max-width: 48rem;
-		text-align: center;
-	}
-
-	.modal-actions {
-		padding-top: 0.5rem;
-		margin-top: 2rem;
+		min-width: 24rem;
+		width: 100%;
+		max-width: 40rem;
 		text-align: center;
 	}
 </style>
