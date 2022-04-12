b129c8ac456d1cc66781d9bd5c79869d159904e5
Georgios Jason Efstathiou <j@jason-e.dev>
2022-04-12 15:14:05 +0200

    style

--- src/lib/stores/workstreams/types.ts
+++ src/lib/stores/workstreams/types.ts
@@ -1,56 +1,56 @@
 export enum WorkstreamState {
-  RFA = "rfa", // Request for Applications. Initial state of Workstream
-  ACTIVE = "active",
-  CLOSED = "closed",
-  CANCELLED = "cancelled",
+    RFA = "rfa", // Request for Applications. Initial state of Workstream
+    ACTIVE = "active",
+    CLOSED = "closed",
+    CANCELLED = "cancelled",
 }
 
 export enum WorkstreamType {
-  GRANT = "grant",
-  ROLE = "role",
+    GRANT = "grant",
+    ROLE = "role",
 }
 
 export enum Currency {
-  DAI = "dai",
+    DAI = "dai",
 }
 
 export interface Timestamp {
-	_seconds: number;
-	_nanoseconds: number;
+    _seconds: number;
+    _nanoseconds: number;
 }
 
 export interface WorkstreamBase {
-  id: string;
-  state: WorkstreamState;
-  creator: string;
-  created_at: Timestamp;
-  payment_rate?: number;
-  payment_currency?: Currency;
-  title: string;
-  desc: string;
+    id: string;
+    state: WorkstreamState;
+    creator: string;
+    created_at: Timestamp;
+    payment_rate?: number;
+    payment_currency?: Currency;
+    title: string;
+    desc: string;
 }
 
 export interface Grant extends WorkstreamBase {
-  type: WorkstreamType.GRANT;
-  length?: number;
+    type: WorkstreamType.GRANT;
+    length?: number;
 }
 
 export interface Role extends WorkstreamBase {
-  type: WorkstreamType.ROLE;
+    type: WorkstreamType.ROLE;
 }
 
 export type Workstream = Grant | Role;
 
 export type Application = {
-	title: string;
-	proposal_id: number;
-	desc: string;
-	creator: string;
-	recipients: Array<string>;
-	payment_rate: number;
-	currency: string;
-	created_at: number;
-	ending_at: number;
-	workstream_id: string;
-	state: string;
+    title: string;
+    proposal_id: number;
+    desc: string;
+    creator: string;
+    recipients: Array<string>;
+    payment_rate: number;
+    currency: string;
+    created_at: number;
+    ending_at: number;
+    workstream_id: string;
+    state: string;
 };
