0187008d46665d47a84507d7c90cb6b94e996cc7
Georgios Jason Efstathiou <j@jason-e.dev>
2022-04-13 19:31:49 +0200

    separate auth store

--- src/lib/components/Connect.svelte
+++ src/lib/components/Connect.svelte
@@ -2,25 +2,33 @@
 	import { formatAddress } from '$lib/utils/format';
 	import { walletStore } from '$lib/stores/wallet/wallet';
 	import Button from '$components/Button.svelte';
+	import { authStore } from '$lib/stores/auth/auth';
 
-	$: label = $walletStore.initialized && formatAddress($walletStore.address);
+	$: label = $walletStore.connected && formatAddress($walletStore.address);
 
-	$: {
-		if ($walletStore.initialized && !$walletStore.authenticated) {
-			walletStore.authenticate();
-		}
+	let locked: boolean;
+
+	async function logIn() {
+		locked = true;
+		try {
+			if (!$walletStore.connected) await walletStore.connect();
+			if (!$authStore.authenticated) await authStore.authenticate($walletStore);
+		} catch {}
+		locked = false;
 	}
 </script>
 
-{#if $walletStore.initialized}
+{#if $walletStore.connected && $authStore.authenticated}
 	<Button
 		variant="outline"
 		on:click={() => walletStore.disconnect()}
 		on:blur={() => console.log('blur')}
 		on:focus={() => console.log('focus')}
-		on:mouseout={() => (label = formatAddress($walletStore.initialized && $walletStore.address))}
+		on:mouseout={() => (label = formatAddress($walletStore.connected && $walletStore.address))}
 		on:mouseover={() => (label = 'disconnect')}>{label}</Button
 	>
+{:else if locked }
+	<Button disabled variant="outline">. . .</Button>
 {:else}
-	<Button on:click={() => walletStore.connect()} variant="outline">connect</Button>
+	<Button on:click={() => logIn()} variant="outline">Log in</Button>
 {/if}
--- src/lib/stores/wallet/wallet.ts
+++ src/lib/stores/wallet/wallet.ts
@@ -1,60 +1,16 @@
 import { browser } from '$app/env';
-import { getConfig } from '$lib/config';
 import { ethers, Signer } from 'ethers';
-import { SiweMessage } from 'siwe';
+import { get, writable } from 'svelte/store';
 
-import { writable } from 'svelte/store';
-
-type WalletData = {
-  initialized: true;
+export type WalletData = {
+  connected: true;
   connecting: false;
-  authenticated: boolean;
   signer: Signer;
   provider: ethers.providers.Web3Provider;
   address: string;
 } | {
-  initialized: false;
+  connected: false;
   connecting: boolean;
-  authenticated?: boolean;
-}
-
-const BACKEND_URL_BASE = getConfig().API_URL_BASE;
-
-async function createSiweMessage(address: string, statement: string) {
-  const res = await fetch(`${BACKEND_URL_BASE}/nonce`, { credentials: "include" });
-
-  const nextWeek = new Date();
-  nextWeek.setDate(nextWeek.getDate() + 7);
-
-  const message = new SiweMessage({
-      domain: window.location.host,
-      address: ethers.utils.getAddress(address),
-      statement,
-      uri: origin,
-      expirationTime: nextWeek.toISOString(), 
-      version: '1',
-      chainId: 1,
-      nonce: await res.text()
-  });
-  return message.prepareMessage();
-}
-
-async function sendSignatureForVerification(message: string, signature: string) {
-  const res = await fetch(`${BACKEND_URL_BASE}/login`, {
-      method: "POST",
-      headers: {
-          'Content-Type': 'application/json',
-      },
-      credentials: "include",
-      body: JSON.stringify({ message, signature }),
-  });
-  
-  return res;
-}
-
-
-function getCookieValue(name) {
-  return document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)')?.pop() || '';
 }
 
 function updateAccounts(walletData: WalletData, accounts: string[]): WalletData {
@@ -62,16 +18,15 @@ function updateAccounts(walletData: WalletData, accounts: string[]): WalletData 
     const provider = new ethers.providers.Web3Provider(window.ethereum, "rinkeby");
 
     return {
-      initialized: true,
-      authenticated: getCookieValue("auth") === "true",
-      connecting: walletData.initialized && walletData.connecting || false,
+      connected: true,
+      connecting: walletData.connected && walletData.connecting || false,
       address: accounts[0],
       provider,
       signer: provider.getSigner(),
     };
   } else {
     return {
-      initialized: false,
+      connected: false,
       connecting: false,
     }
   }
@@ -79,7 +34,7 @@ function updateAccounts(walletData: WalletData, accounts: string[]): WalletData 
 
 export const walletStore = (() => {
 	const { subscribe, update, set } = writable<WalletData>({
-    initialized: false,
+    connected: false,
     connecting: false,
   });
 
@@ -93,66 +48,41 @@ export const walletStore = (() => {
     });
   }
 
-  function connect(current: WalletData): WalletData {
-    const provider = current.initialized
-      ? current.provider
+  async function connect(): Promise<WalletData> {
+    const walletData = get(walletStore);
+
+    const provider = walletData.connected
+      ? walletData.provider
       : new ethers.providers.Web3Provider(window.ethereum, "rinkeby"); 
 
     set({
-      initialized: false,
+      connected: false,
       connecting: true,
     });
 
-    provider.send('eth_requestAccounts', [])
-      .then((result) => set({
-        initialized: true,
-        authenticated: false,
-        connecting: false,
-        address: result[0],
-        provider,
-        signer: provider.getSigner(),
-      }))
-      .catch(() => console.error("Error while connecting to wallet"));
+    const accounts = await provider.send('eth_requestAccounts', [])
 
-    return current;
+    set({
+      connected: true,
+      connecting: false,
+      address: accounts[0],
+      provider,
+      signer: provider.getSigner(),
+    });
+
+    return get(walletStore);
   }
 
   function disconnect() {
     set({
-      initialized: false,
+      connected: false,
       connecting: false,
     });
   }
 
-  function authenticate(walletData: WalletData): WalletData {
-    if (!walletData.initialized) throw new Error("Initialize Wallet first");
-
-    (async () => {
-      const message = await createSiweMessage(
-        walletData.address,
-        "Please sign this message to log in to Radicle Workstreams. Since this"
-        + " is not a transaction, there will be no transaction costs."
-      );
-  
-      const signature = await walletData.signer.signMessage(message);
-
-      const result = await sendSignatureForVerification(message, signature);
-
-      if (result.status === 200) {
-        update((walletData) => ({
-          ...walletData,
-          authenticated: true,
-        }));
-      }
-    })();
-
-    return walletData;
-  }
-
   return {
     subscribe,
-    connect: () => update((walletData) => connect(walletData)),
+    connect,
     disconnect,
-    authenticate: () => update((walletData) => authenticate(walletData)),
   }
 })();
