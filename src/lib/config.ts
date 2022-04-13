06db593c33774cc0dee444b347badfea4bc6848f
Georgios Jason Efstathiou <j@jason-e.dev>
2022-04-13 16:11:03 +0200

    use prod API even on local

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -7,12 +7,11 @@ const configMap = {
     API_URL_BASE: "https://us-central1-radicle-workstreams.cloudfunctions.net/api",
   },
   development: {
-    API_URL_BASE: "http://127.0.0.1:5001/radicle-workstreams/us-central1/api",
+    API_URL_BASE: "https://us-central1-radicle-workstreams.cloudfunctions.net/api",
   }
 }
 
 function getConfig(): Config {
-  console.log(process.env.NODE_ENV);
   return process.env.NODE_ENV === "production"
     ? configMap.production
     : configMap.development
