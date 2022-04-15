b670c58b2335740ba0f9055e758513ee4c26f375
Julien Donck <hello@juliendonck.com>
2022-04-15 09:11:29 +0200

    Add apply button to workstream detail page
    Signed-off-by: Julien Donck <hello@juliendonck.com>

--- src/lib/components/WorkstreamDetail.svelte
+++ src/lib/components/WorkstreamDetail.svelte
@@ -2,6 +2,8 @@
 	import { timeframeFormat, dateFormat } from '$lib/utils/format';
 	import User from '$components/User.svelte';
 	import Tag from '$components/Tag.svelte';
+	import Apply from '$components/icons/Ledger.svelte';
+	import Button from '$components/Button.svelte';
 	import Markdown from '$components/Markdown.svelte';
 	import type { Workstream } from '$lib/stores/workstreams/types';
 
@@ -20,17 +22,18 @@
 			<span class="label" style="margin-left: 0.5rem;">on {dateFormat(workstream.created_at)}</span>
 		</div>
 		<div class="timerate">
-			{#if workstream.type === 'grant' && workstream.duration}
-				<div>
-					<p class="timeframe">{timeframeFormat(workstream.duration)}</p>
-				</div>
-			{/if}
 			<div style="text-align: right;">
 				<p class="typo-text-bold rate">
 					{Math.floor(workstream.payment.rate)}
 					{workstream.payment.currency} <span class="typo-regular">/ day</span>
 				</p>
 			</div>
+			{#if workstream.type === 'grant' && workstream.duration}
+				<div>
+					<p class="timeframe">{timeframeFormat(workstream.duration)}</p>
+				</div>
+			{/if}
+			<Button><Apply />Apply</Button>
 		</div>
 		<div>
 			<div class="desc">
@@ -68,13 +71,15 @@
 	.timerate {
 		display: flex;
 		justify-content: space-between;
+		align-items: center;
+		padding: 1rem;
+		border: 1px solid var(--color-foreground-level-3);
+		border-radius: 1rem;
 	}
 	.timeframe {
-		margin-top: 0.5rem;
-		color: var(--color-foreground);
+		color: var(--color-foreground-level-6);
 	}
 	.rate {
-		margin-top: 0.5rem;
 		color: var(--color-primary);
 	}
 </style>
