fad1a19d4da733f77bef0a2b71fa21dde908cdea
Georgios Jason Efstathiou <j@jason-e.dev>
2022-04-19 10:56:25 +0200

    add store for connected and logged in

--- src/lib/components/Connect.svelte
+++ src/lib/components/Connect.svelte
@@ -3,28 +3,28 @@
 	import { walletStore } from '$lib/stores/wallet/wallet';
 	import Button from '$components/Button.svelte';
 	import { authStore } from '$lib/stores/auth/auth';
+	import connectedAndLoggedIn from '$lib/stores/connectedAndLoggedIn';
 
 	$: label = $walletStore.connected && formatAddress($walletStore.address);
 
-	$: connectedAndLoggedIn =
-		$walletStore.connected &&
-		$authStore.authenticated &&
-		$walletStore.address === $authStore.address;
-
 	let locked: boolean;
+
+	$: {
+		console.log($connectedAndLoggedIn);
+	}
 
 	async function logIn() {
 		locked = true;
 		try {
 			if (!$walletStore.connected) await walletStore.connect();
-			if (!connectedAndLoggedIn) await authStore.authenticate($walletStore);
+			if (!$connectedAndLoggedIn) await authStore.authenticate($walletStore);
 		} finally {
 			locked = false;
 		}
 	}
 </script>
 
-{#if connectedAndLoggedIn}
+{#if $connectedAndLoggedIn}
 	<Button variant="outline" on:click={() => walletStore.disconnect()}>{label}</Button>
 {:else if locked}
 	<Button disabled variant="outline">. . .</Button>
