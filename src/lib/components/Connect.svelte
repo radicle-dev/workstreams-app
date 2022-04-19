ac6fd4da4940e1da75ed6c90e496b46b62766679
Georgios Jason Efstathiou <j@jason-e.dev>
2022-04-19 11:44:56 +0200

    make disconnect button less glitchly

--- src/lib/components/Connect.svelte
+++ src/lib/components/Connect.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { walletStore } from '$lib/stores/wallet/wallet';
 	import Button from '$components/Button.svelte';
+	import { fade } from 'svelte/transition';
 	import User from '$components/User.svelte';
 	import { authStore } from '$lib/stores/auth/auth';
 	import connectedAndLoggedIn from '$lib/stores/connectedAndLoggedIn';
@@ -20,23 +21,46 @@
 	let hover = false;
 </script>
 
-{#if $connectedAndLoggedIn}
-	<div>
-		<Button
-			variant="outline"
-			on:click={() => walletStore.disconnect()}
-			on:mouseenter={() => (hover = true)}
-			on:mouseleave={() => (hover = false)}
-		>
-			{#if hover}
-				Sign out
-			{:else}
+<div class="connect-button-wrapper" on:mouseleave={() => (hover = false)}>
+	{#if $connectedAndLoggedIn}
+		{#if hover}
+			<div
+				on:click={() => walletStore.disconnect()}
+				transition:fade={{ duration: 100 }}
+				class="log-out-overlay"
+			>
+				<h4>Log out</h4>
+			</div>
+		{/if}
+		<div>
+			<Button variant="outline" on:mouseenter={() => (hover = true)}>
 				<User address={$authStore.address} />
-			{/if}
-		</Button>
-	</div>
-{:else if locked}
-	<Button disabled variant="outline">. . .</Button>
-{:else}
-	<Button on:click={() => logIn()} variant="outline">Sign in with Ethereum</Button>
-{/if}
+			</Button>
+		</div>
+	{:else if locked}
+		<Button disabled variant="outline">. . .</Button>
+	{:else}
+		<Button on:click={() => logIn()} variant="outline">Sign in with Ethereum</Button>
+	{/if}
+</div>
+
+<style>
+	.connect-button-wrapper {
+		position: relative;
+	}
+	.log-out-overlay {
+		position: absolute;
+		top: 0;
+		background-color: var(--color-background);
+		left: 0;
+		z-index: 5;
+		height: calc(100% - 4px);
+		width: calc(100% - 4px);
+		margin: 2px;
+		border-radius: 4px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		cursor: pointer;
+	}
+</style>
