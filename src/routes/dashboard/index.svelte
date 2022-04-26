8aebbf7e14382460046f5b383942cd0bca0b6849
Georgios Jason Efstathiou <j@jason-e.dev>
2022-04-26 16:19:14 +0200

    remove ws type filter from dashbaord

--- src/routes/dashboard/index.svelte
+++ src/routes/dashboard/index.svelte
@@ -1,49 +1,27 @@
 <script lang="ts">
-  import type { Workstream } from '$lib/stores/workstreams/types';
-  import * as modal from '$lib/utils/modal';
-  import CreateModal from '$components/CreateModal.svelte';
-  import SegmentedControl from 'radicle-design-system/SegmentedControl.svelte';
-  import Button from 'radicle-design-system/Button.svelte';
-  import TokenStreamsIcon from 'radicle-design-system/icons/TokenStreams.svelte';
-  import WorkstreamCard from '$components/WorkstreamCard.svelte';
-  import { getConfig } from '$lib/config';
-  import { walletStore } from '$lib/stores/wallet/wallet';
-  import { authStore } from '$lib/stores/auth/auth';
-  import { browser } from '$app/env';
-  import EmptyState from '$lib/components/EmptyState.svelte';
-  import connectedAndLoggedIn from '$lib/stores/connectedAndLoggedIn';
+	import type { Workstream } from '$lib/stores/workstreams/types';
+	import WorkstreamCard from '$components/WorkstreamCard.svelte';
+	import { getConfig } from '$lib/config';
+	import { walletStore } from '$lib/stores/wallet/wallet';
+	import { authStore } from '$lib/stores/auth/auth';
+	import { browser } from '$app/env';
+	import EmptyState from '$lib/components/EmptyState.svelte';
+	import connectedAndLoggedIn from '$lib/stores/connectedAndLoggedIn';
 
   let createdWorkstreams: Workstream[] = [];
   let appliedToWorkstreams: Workstream[] = [];
 
   let locked: boolean;
 
-  const applicationOptions = [
-    {
-      title: 'All',
-      value: 'all'
-    },
-    {
-      title: 'Role',
-      value: 'role'
-    },
-    {
-      title: 'Grant',
-      value: 'grant'
-    }
-  ];
-
-  $: {
-    if ($walletStore.connected && $authStore.authenticated) {
-      (async () => {
-        const result = await fetch(
-          `${getConfig().API_URL_BASE}/workstreams?createdBy=${
-            $walletStore.address
-          }`,
-          {
-            credentials: 'include'
-          }
-        );
+	$: {
+		if ($walletStore.connected && $authStore.authenticated) {
+			(async () => {
+				const result = await fetch(
+					`${getConfig().API_URL_BASE}/workstreams?createdBy=${$walletStore.address}`,
+					{
+						credentials: 'include'
+					}
+				);
 
         createdWorkstreams = await result.json();
       })();
