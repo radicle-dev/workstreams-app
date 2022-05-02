938593b7b5e668391964c9c79848041192325bbc
Julien Donck <hello@juliendonck.com>
2022-05-02 13:33:52 +0200

    add responsiveness to index
    Signed-off-by: Julien Donck <hello@juliendonck.com>

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -37,7 +37,22 @@
 <style>
   .overview {
     display: grid;
-    grid-template-columns: repeat(3, minmax(0, 1fr));
+    grid-template-columns: repeat(2, minmax(0, 1fr));
     gap: 1.5rem;
+  }
+
+  @media only screen and (min-width: 75rem) {
+    .overview {
+      display: grid;
+      grid-template-columns: repeat(3, minmax(0, 1fr));
+      gap: 1.5rem;
+    }
+  }
+  @media only screen and (max-width: 54rem) {
+    .overview {
+      display: grid;
+      grid-template-columns: repeat(1, minmax(0, 1fr));
+      gap: 1.5rem;
+    }
   }
 </style>
