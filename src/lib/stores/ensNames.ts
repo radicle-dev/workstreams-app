b1f5e461af06a1f69636f190a4c4de0f98099730
Georgios Jason Efstathiou <j@jason-e.dev>
2022-05-04 10:13:11 +0200

    wait for wallet store to be initialized before showing app

--- src/lib/stores/ensNames.ts
+++ src/lib/stores/ensNames.ts
@@ -12,35 +12,31 @@ export default (() => {
     address: string,
     provider?: ethers.providers.BaseProvider
   ) {
-    const saved = get(store)[address];
+    /*
+      Write an empty object initially in order to prevent
+      multiple in-flight requests for the same address.
+    */
+    store.update((v) => ({ ...v, [address]: {} }));
 
-    if (!saved) {
-      /*
-        Write an empty object initially in order to prevent
-        multiple in-flight requests for the same address.
-      */
-      store.update((v) => ({ ...v, [address]: {} }));
+    const name = await (provider || defaultProvider).lookupAddress(address);
 
-      const name = await (provider || defaultProvider).lookupAddress(address);
+    /*
+      Updating with only the name already since the avatar
+      tends to be slow.
+    */
+    store.update((v) => ({ ...v, [address]: { name } }));
 
-      /*
-        Updating with only the name already since the avatar
-        tends to be slow.
-      */
-      store.update((v) => ({ ...v, [address]: { name } }));
+    let pic: string;
 
-      let pic: string;
-
-      if (name) {
-        pic = (
-          await (
-            await (provider || defaultProvider).getResolver(name)
-          ).getAvatar()
-        )?.url;
-      }
-
-      store.update((v) => ({ ...v, [address]: { name, pic } }));
+    if (name) {
+      pic = (
+        await (
+          await (provider || defaultProvider).getResolver(name)
+        ).getAvatar()
+      )?.url;
     }
+
+    store.update((v) => ({ ...v, [address]: { name, pic } }));
   }
 
   return {
