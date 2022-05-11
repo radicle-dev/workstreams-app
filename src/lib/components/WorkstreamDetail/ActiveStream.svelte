c648dbdbdea0bc1b9c653e71ba0e07921f9bf235
Julien Donck <hello@juliendonck.com>
2022-05-11 15:47:09 +0200

    new icon + updated design system
    Signed-off-by: Julien Donck <hello@juliendonck.com>

--- src/lib/components/WorkstreamDetail/ActiveStream.svelte
+++ src/lib/components/WorkstreamDetail/ActiveStream.svelte
@@ -6,7 +6,7 @@
   import Rate from '$components/Rate.svelte';
   import Row from '$components/Row.svelte';
   import ApplicationModal from '$components/ApplicationModal.svelte';
-  import Cross from 'radicle-design-system/icons/Cross.svelte';
+  import Pause from 'radicle-design-system/icons/Pause.svelte';
   import Button from 'radicle-design-system/Button.svelte';
   import type { Application, Workstream } from '$lib/stores/workstreams/types';
 
@@ -48,7 +48,7 @@
     <div class="stream-actions">
       <p>5000 of 8000 DAI topped up</p>
       <div style="display: flex; gap: .75rem;">
-        <Button variant="primary-outline" icon={Cross}>Pause</Button>
+        <Button variant="primary-outline" icon={Pause}>Pause</Button>
         <Button>Top up</Button>
       </div>
     </div>
