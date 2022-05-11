5d51a878411d340707335fa8f638a6de0e1f76f7
Julien Donck <hello@juliendonck.com>
2022-05-11 11:04:50 +0200

    clean up detail page
    Signed-off-by: Julien Donck <hello@juliendonck.com>

--- src/lib/components/WorkstreamDetail/index.svelte
+++ src/lib/components/WorkstreamDetail/index.svelte
@@ -1,21 +1,12 @@
 <script lang="ts">
   import { dateFormat } from '$lib/utils/format';
-  import * as modal from '$lib/utils/modal';
   import { walletStore } from '$lib/stores/wallet/wallet';
   import connectedAndLoggedIn from '$lib/stores/connectedAndLoggedIn';
 
   import ApplicationList from './ApplicationList.svelte';
-  import Card from '$components/Card.svelte';
+  import ActiveStream from './ActiveStream.svelte';
+  import ApplyRow from './ApplyRow.svelte';
   import User from '$components/User.svelte';
-  import Rate from '$components/Rate.svelte';
-  import Row from '$components/Row.svelte';
-  import TimeRate from '$components/TimeRate.svelte';
-  import ApplyModal from '$components/ApplyModal.svelte';
-  import ApplicationModal from '$components/ApplicationModal.svelte';
-  import Apply from 'radicle-design-system/icons/Ledger.svelte';
-  import Cross from 'radicle-design-system/icons/Cross.svelte';
-  import Button from 'radicle-design-system/Button.svelte';
-  import Tooltip from 'radicle-design-system/Tooltip.svelte';
   import Markdown from 'radicle-design-system/Markdown.svelte';
   import {
     WorkstreamState,
@@ -64,46 +55,7 @@
       >
     </div>
     {#if workstream.state === WorkstreamState.ACTIVE}
-      <Card hoverable={false}>
-        <div slot="top">
-          <h3 style="margin-bottom: 1rem;">Active stream</h3>
-          <div class="timerate">
-            <div style="text-align: right;">
-              <Rate
-                ratePerSecond={workstream.ratePerSecond}
-                total={workstream.total}
-              />
-            </div>
-            <div>
-              <p class="timeframe">Active since Jan 5, 2022</p>
-            </div>
-          </div>
-        </div>
-        <div slot="bottom">
-          <Row>
-            <div slot="left">
-              <User address={acceptedApplication.creator} />
-            </div>
-            <div slot="right" class="row-actions">
-              <p class="proposal">4020 DAI left (8.04 days)</p>
-              <Button
-                on:click={() =>
-                  modal.show(ApplicationModal, undefined, {
-                    workstream,
-                    acceptedApplication
-                  })}>View application</Button
-              >
-            </div>
-          </Row>
-          <div class="stream-actions">
-            <p>5000 of 8000 DAI topped up</p>
-            <div style="display: flex; gap: .75rem;">
-              <Button variant="primary-outline" icon={Cross}>Pause</Button>
-              <Button>Top up</Button>
-            </div>
-          </div>
-        </div>
-      </Card>
+      <ActiveStream {workstream} {acceptedApplication} />
     {:else if workstream.state === WorkstreamState.PENDING && acceptedApplication}
       <ApplicationList
         applications={[acceptedApplication]}
@@ -113,25 +65,7 @@
         accepted={creator}
       />
     {:else}
-      <Card hoverable={false} style="margin-bottom: 1.5rem;">
-        <div slot="top">
-          <div class="timerate">
-            <TimeRate {workstream} />
-            {#if !creator}
-              <Tooltip value={applied ? "You've already applied" : null}>
-                <Button
-                  disabled={applied || !$connectedAndLoggedIn}
-                  icon={Apply}
-                  on:click={() =>
-                    modal.show(ApplyModal, undefined, { workstream })}
-                >
-                  Apply
-                </Button>
-              </Tooltip>
-            {/if}
-          </div>
-        </div>
-      </Card>
+      <ApplyRow {workstream} {creator} {applied} />
     {/if}
     {#if workstream.state === WorkstreamState.RFA && openApplications?.length > 0}
       <ApplicationList
@@ -175,35 +109,9 @@
     margin-right: 0.5rem;
   }
 
-  .proposal {
-    display: flex;
-    color: var(--color-primary);
-    gap: 0.5rem;
-  }
-  .row-actions {
-    display: flex;
-    gap: 1rem;
-    align-items: center;
-  }
   .desc {
     margin-top: 2rem;
     color: var(--color-foreground);
     user-select: text;
-  }
-  .timerate {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    gap: 1.5rem;
-  }
-  .timeframe {
-    color: var(--color-foreground-level-6);
-  }
-
-  .stream-actions {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-top: 1rem;
   }
 </style>
