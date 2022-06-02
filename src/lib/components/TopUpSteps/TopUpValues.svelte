0cddfb5074a7c13efe477fa9013700ba1566102f
Georgios Jason Efstathiou <j@jason-e.dev>
2022-06-02 16:16:52 +0200

    style topup modal

--- src/lib/components/TopUpSteps/TopUpValues.svelte
+++ src/lib/components/TopUpSteps/TopUpValues.svelte
@@ -6,10 +6,14 @@
     workstreamsStore,
     type EnrichedWorkstream
   } from '$lib/stores/workstreams';
-  import TextInput from '../TextInput.svelte';
   import { createEventDispatcher, onMount } from 'svelte';
   import Button from 'radicle-design-system/Button.svelte';
   import drips from '$lib/stores/drips';
+  import Card from '../Card.svelte';
+  import TitleMeta from '../TitleMeta.svelte';
+  import TimeRate from '../TimeRate.svelte';
+  import { currencyFormat } from '$lib/utils/format';
+  import TextInput from 'radicle-design-system/TextInput.svelte';
 
   const dispatch = createEventDispatcher();
 
@@ -20,11 +24,22 @@
   let topUpAmount = 0;
   $: topUpAmountWei = toWei(topUpAmount || 0).toBigInt();
 
-  $: streamingUntil =
-    $estimates.streams[enrichedWorkstream.data.id]?.streamingUntil;
+  $: estimate = $estimates.streams[enrichedWorkstream.data.id];
+  $: streamingUntil = estimate?.streamingUntil;
   $: currAmtPerSec = enrichedWorkstream.onChainData.amtPerSec.wei;
+  $: remainingBalance = estimate?.remainingBalance;
 
   let streamingUntilAfterTopup: Date | undefined = undefined;
+
+  const formatDate = (date: Date) =>
+    Intl.DateTimeFormat('en-US', {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: 'numeric',
+      second: '2-digit'
+    }).format(date);
 
   function updateStreamingUntilAfterTopup() {
     /*
@@ -85,21 +100,50 @@
   <Emoji emoji="💰" size="large" />
   <h1>Top up</h1>
   <div class="content">
-    <p>Workstream is streaming until: {streamingUntil}</p>
-    {#if streamingUntil.getTime() < new Date().getTime()}
-      The workstream is currently out of funds and not streaming anymore!
-    {/if}
     <div class="input-with-label">
-      <p>Top up amount:</p>
-      <TextInput number bind:value={topUpAmount} />
+      <h4>Topping up</h4>
+      <Card style="width: 100%">
+        <div slot="top">
+          <TitleMeta workstream={enrichedWorkstream.data} />
+        </div>
+        <div slot="bottom">
+          <TimeRate workstream={enrichedWorkstream.data} />
+        </div>
+      </Card>
     </div>
-    <p>
-      After topping up {topUpAmount || 0} DAI, the workstream will run until approximately:
-      {streamingUntilAfterTopup}
-    </p>
-    <Button disabled={buttonDisabled} on:click={topUp}
-      >Top up {topUpAmount} DAI</Button
-    >
+    <div class="row">
+      <div class="input-with-label narrow">
+        <h4>Remaining balance</h4>
+        <p class="value">{currencyFormat(remainingBalance)} DAI</p>
+      </div>
+      <div class="input-with-label">
+        {#if remainingBalance.wei > BigInt(0)}
+          <h4>Streaming until</h4>
+        {:else}
+          <h4>Ran out of funds on</h4>
+        {/if}
+        <p class="value">{formatDate(streamingUntil)}</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="input-with-label amount-input narrow">
+        <h4>Top up amount</h4>
+        <TextInput
+          variant={{ type: 'number', min: 0 }}
+          bind:value={topUpAmount}
+          suffix="DAI"
+        />
+      </div>
+      <div class="input-with-label">
+        <h4>After topup, runs until</h4>
+        <p class="value">{formatDate(streamingUntilAfterTopup)}</p>
+      </div>
+    </div>
+    <div class="actions">
+      <Button disabled={buttonDisabled} on:click={topUp}
+        >Top up {topUpAmount} DAI</Button
+      >
+    </div>
   </div>
 </div>
 
@@ -108,14 +152,47 @@
     margin: 1rem 0;
   }
 
+  h4 {
+    color: var(--color-foreground-level-5);
+  }
+
   .content {
     display: flex;
+    align-items: flex-start;
     flex-direction: column;
-    gap: 0.5rem;
+    gap: 2rem;
   }
 
   .input-with-label {
     display: flex;
-    gap: 8px;
+    width: 100%;
+    align-items: flex-start;
+    flex-direction: column;
+    gap: 0.75rem;
+    flex: 1;
+  }
+
+  .narrow {
+    flex: 0.6;
+  }
+
+  .value {
+    font-size: 1.5rem;
+    height: 2.5rem;
+    line-height: 2.5rem;
+    text-align: left;
+    color: var(--color-foreground-level-6);
+  }
+
+  .actions {
+    display: flex;
+    justify-content: flex-end;
+    width: 100%;
+  }
+
+  .row {
+    display: flex;
+    gap: 2rem;
+    width: 100%;
   }
 </style>
