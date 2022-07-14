ea53cf9d09c92e1108c133efda77b922267f3fe4
Georgios Jason Efstathiou <j@jason-e.dev>
2022-07-14 15:28:02 +0200

    fix: apply button appears when it shouldnt

--- src/lib/components/WorkstreamDetail/ApplyRow.svelte
+++ src/lib/components/WorkstreamDetail/ApplyRow.svelte
@@ -1,38 +1,17 @@
 <script lang="ts">
-  import * as modal from '$lib/utils/modal';
-
   import Card from '$components/Card.svelte';
   import TimeRate from '$components/TimeRate.svelte';
-  import ApplyModal from '$components/ApplyModal.svelte';
-  import Apply from 'radicle-design-system/icons/Ledger.svelte';
-  import Button from 'radicle-design-system/Button.svelte';
-  import Tooltip from 'radicle-design-system/Tooltip.svelte';
   import type { Workstream } from '$lib/stores/workstreams/types';
-  import { walletStore } from '$lib/stores/wallet/wallet';
+  import ApplyButton from '../ApplyButton.svelte';
 
   export let workstream: Workstream;
-  export let creator = false;
-  export let applied = false;
 </script>
 
 <Card hoverable={false} style="margin-bottom: 1.5rem;">
   <div slot="top">
     <div class="timerate">
       <TimeRate {workstream} />
-      {#if !creator}
-        <Tooltip
-          position="top"
-          value={applied ? "You've already applied" : null}
-        >
-          <Button
-            disabled={applied || !$walletStore.ready}
-            icon={Apply}
-            on:click={() => modal.show(ApplyModal, undefined, { workstream })}
-          >
-            Apply
-          </Button>
-        </Tooltip>
-      {/if}
+      <ApplyButton {workstream} />
     </div>
   </div>
 </Card>
