a00ba6fd086c5151f9052b65e80a17a8e834c7bd
Georgios Jason Efstathiou <j@jason-e.dev>
2022-07-25 14:37:52 +0200

    chore: update svelte deps

--- svelte.config.js
+++ svelte.config.js
@@ -1,31 +1,13 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
   preprocess: preprocess(),
-
   kit: {
-    adapter: adapter(),
-    vite: {
-      resolve: {
-        alias: {
-          $components: path.resolve('./src/lib/components')
-        }
-      },
-      optimizeDeps: {
-        esbuildOptions: {
-          // Node.js global to browser globalThis
-          define: {
-            global: 'globalThis',
-            'process.env': '{ "READABLE_STREAM": "disable" }'
-          }
-        }
-      }
-    }
+    adapter: adapter()
   }
 };
 
---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import path from 'path';
+
+/** @type {import('vite').UserConfig} */
+const config = {
+  plugins: [sveltekit()],
+  resolve: {
+    alias: {
+      $components: path.resolve('./src/lib/components'),
+      util: 'util',
+      stream: 'stream-browserify'
+    }
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis',
+        'process.env': '{ "READABLE_STREAM": "disable" }'
+      }
+    }
+  }
+};
+
+export default config;
