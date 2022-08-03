6db008556efd835735401bb6374b1f2c1fd84d12
Jason Efstathiou <j@jason-e.dev>
2022-08-03 12:05:40 +0200

    feat: allow picking safes from a dropdown if there is more than one (#165)

--- src/lib/components/Connect/steps/LinkSafe.svelte
+++ src/lib/components/Connect/steps/LinkSafe.svelte
@@ -2,23 +2,26 @@
   import { createEventDispatcher, onMount } from 'svelte';
   import Button from 'radicle-design-system/Button.svelte';
   import Spinner from 'radicle-design-system/Spinner.svelte';
+  import Dropdown from 'radicle-design-system/Dropdown.svelte';
   import { get } from 'svelte/store';
 
   import * as modal from '$lib/utils/modal';
   import type { AwaitPendingPayload } from '$lib/components/StepperModal/types';
-  import User from '$lib/components/User.svelte';
   import { walletStore } from '$lib/stores/wallet/wallet';
   import { getSafesForAddress } from '$lib/api/gnosis';
   import clearStores from '$lib/stores/utils/clearStores';
   import connectStores from '$lib/stores/utils/connectStores';
   import StepContent from '$lib/components/StepContent.svelte';
+  import { formatAddress } from '$lib/utils/format';
+  import User from '$lib/components/User.svelte';
 
   const dispatch = createEventDispatcher<{
     continue: never;
     awaitPending: AwaitPendingPayload;
   }>();
 
-  let safeAddress: string | undefined = undefined;
+  let safeAddresses: string[] | undefined = undefined;
+  let selectedSafeAddress: string;
 
   onMount(async () => {
     const ownedSafes = await getSafesForAddress(
@@ -30,12 +33,13 @@
       modal.hide();
     }
 
-    safeAddress = ownedSafes[0];
+    safeAddresses = ownedSafes;
+    selectedSafeAddress = safeAddresses[0];
   });
 
-  function linkSafe() {
+  function linkSafe(address: string) {
     const waitFor = async () => {
-      await walletStore.linkSafe(safeAddress);
+      await walletStore.linkSafe(address);
       await walletStore.connectSafe();
 
       const ws = get(walletStore);
@@ -54,11 +58,23 @@
   }
 </script>
 
-{#if safeAddress}
+{#if safeAddresses}
   <StepContent>
     <span slot="headline">Link your Gnosis Safe</span>
     <div slot="content">
-      <div class="safe"><User address={safeAddress} /></div>
+      {#if safeAddresses.length > 1}
+        <div class="safe-selector">
+          <Dropdown
+            bind:value={selectedSafeAddress}
+            options={safeAddresses.map((a) => ({
+              title: formatAddress(a),
+              value: a
+            }))}
+          />
+        </div>
+      {:else}
+        <div class="safe"><User address={selectedSafeAddress} /></div>
+      {/if}
       <p>
         We detected a Gnosis Safe associated with your account. If you link it,
         you'll be interacting with Workstreams on behalf of your safe, and all
@@ -84,7 +100,7 @@
     <div slot="step-actions">
       <Button variant="outline" on:click={() => modal.hide()}>Don't link</Button
       >
-      <Button on:click={linkSafe}>Link safe</Button>
+      <Button on:click={() => linkSafe(selectedSafeAddress)}>Link safe</Button>
     </div>
   </StepContent>
 {:else}
@@ -95,9 +111,16 @@
   p {
     margin-bottom: 1rem;
   }
+
   .safe {
     margin-bottom: 1.5rem;
     display: flex;
     justify-content: center;
+  }
+
+  .safe-selector {
+    max-width: 10rem;
+    margin: 0 auto;
+    margin-bottom: 1.5rem;
   }
 </style>
