335801ae9b7a557cab69f1938e50d1e13a78b730
Georgios Jason Efstathiou <j@jason-e.dev>
2022-08-12 12:37:47 +0200

    refactor: split estimation logic into a function that only flattens history

--- src/lib/stores/drips/utils/streamedBetween.ts
+++ src/lib/stores/drips/utils/streamedBetween.ts
@@ -1,10 +1,66 @@
-import type { EnrichedWorkstream } from '$lib/stores/workstreams';
+import type {
+  DripHistoryEvent,
+  EnrichedWorkstream
+} from '$lib/stores/workstreams';
 import { Currency, type Money } from '$lib/stores/workstreams/types';
 import { getUnixTime } from '$lib/utils/time';
 
 interface TimeWindow {
   from: Date;
   to: Date;
+}
+
+/**
+ * Returns the amount of money earned and spent between the given time window
+ * based on a given history of DripHistoryEvents.
+ */
+export function flattenDripHistory(
+  history: DripHistoryEvent[],
+  timeWindow: TimeWindow = { from: new Date(0), to: new Date() }
+) {
+  let amountStreamed = BigInt(0);
+  let amountRemaining = BigInt(0);
+
+  const filteredHistory = history.filter((e) => e.timestamp < timeWindow.to);
+
+  filteredHistory.forEach((e, index) => {
+    const nextEvent = filteredHistory[index + 1];
+    const validUntil = nextEvent?.timestamp || new Date();
+    const { balance, amtPerSec, timestamp: validSince } = e;
+    const validSinceSeconds = getUnixTime(validSince);
+    const validUntilSeconds = getUnixTime(validUntil);
+
+    // Stream is currently paused.
+    if (!nextEvent && amtPerSec.wei === BigInt(0)) {
+      amountRemaining = balance.wei;
+    }
+
+    if (amtPerSec.wei === BigInt(0)) return;
+
+    const toppedUpUntil =
+      validSinceSeconds + Number(balance.wei / amtPerSec.wei);
+
+    const relevantWindow = {
+      from: Math.max(validSinceSeconds, getUnixTime(timeWindow.from)),
+      to: Math.min(getUnixTime(timeWindow.to), validUntilSeconds, toppedUpUntil)
+    };
+
+    const secondsInWindow = Math.floor(
+      Math.max(relevantWindow.to - relevantWindow.from, 0)
+    );
+
+    const streamedDuringCurrentEvent = BigInt(secondsInWindow) * amtPerSec.wei;
+    amountStreamed = amountStreamed + streamedDuringCurrentEvent;
+
+    if (!nextEvent) {
+      amountRemaining = balance.wei - streamedDuringCurrentEvent;
+    }
+  });
+
+  return {
+    amountStreamed,
+    amountRemaining
+  };
 }
 
 /**
@@ -24,47 +80,10 @@ export function streamedBetween(
   for (const stream of streams) {
     const { dripHistory } = stream.onChainData;
 
-    let amountStreamed = BigInt(0);
-    let amountRemaining = BigInt(0);
-
-    dripHistory.forEach((e, index) => {
-      const nextEvent = dripHistory[index + 1];
-      const validUntil = nextEvent?.timestamp || new Date();
-      const { balance, amtPerSec, timestamp: validSince } = e;
-      const validSinceSeconds = getUnixTime(validSince);
-      const validUntilSeconds = getUnixTime(validUntil);
-
-      // Stream is currently paused.
-      if (!nextEvent && amtPerSec.wei === BigInt(0)) {
-        amountRemaining = balance.wei;
-      }
-
-      if (amtPerSec.wei === BigInt(0)) return;
-
-      const toppedUpUntil =
-        validSinceSeconds + Number(balance.wei / amtPerSec.wei);
-
-      const relevantWindow = {
-        from: Math.max(validSinceSeconds, getUnixTime(timeWindow.from)),
-        to: Math.min(
-          getUnixTime(timeWindow.to),
-          validUntilSeconds,
-          toppedUpUntil
-        )
-      };
-
-      const secondsInWindow = Math.floor(
-        Math.max(relevantWindow.to - relevantWindow.from, 0)
-      );
-
-      const streamedDuringCurrentEvent =
-        BigInt(secondsInWindow) * amtPerSec.wei;
-      amountStreamed = amountStreamed + streamedDuringCurrentEvent;
-
-      if (!nextEvent) {
-        amountRemaining = balance.wei - streamedDuringCurrentEvent;
-      }
-    });
+    const { amountStreamed, amountRemaining } = flattenDripHistory(
+      dripHistory,
+      timeWindow
+    );
 
     amountsStreamed.push({
       workstream: stream,
